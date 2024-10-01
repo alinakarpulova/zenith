1d0c4c07c5009993c74ab81283a3dd3ad1c82553
Alina Karpulova <karpulova.alina@gmail.com>
2024-09-30 22:13:22 -0700

    working on adding exercise

--- app/src/main/java/com/example/zenith/activities/screens/exercises/ExerciseNew.java
+++ app/src/main/java/com/example/zenith/activities/screens/exercises/ExerciseNew.java
@@ -1,16 +1,40 @@
 package com.example.zenith.activities.screens.exercises;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.zenith.R;
 
 public class ExerciseNew extends AppCompatActivity {
+    Dialog dialog;
+
 
     @Override
-    public void onCreate(Bundle savedInstance){
+    public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.exercise_new_activity);
+
+        dialog = new Dialog(ExerciseNew.this);
+        dialog.setContentView(R.layout.radio_button_dialog);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.shape_round));
+        dialog.setCancelable(true);
+
+        Button button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
+
     }
 }
+
+
--- app/src/main/java/com/example/zenith/activities/screens/exercises/ExercisesFragment.java
+++ app/src/main/java/com/example/zenith/activities/screens/exercises/ExercisesFragment.java
@@ -1,16 +1,18 @@
 package com.example.zenith.activities.screens.exercises;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.zenith.R;
 
 public class ExercisesFragment extends Fragment {
-
     public ExercisesFragment() {
 
     }
@@ -18,6 +20,20 @@ public class ExercisesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.exercises_fragment, container, false);
+        View view = inflater.inflate(R.layout.exercises_fragment, container, false);
+        Button button = view.findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ExerciseNew.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        return view;
+
     }
 }
