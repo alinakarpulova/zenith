d4432bd511ce3e1cf93255a8c6549e40ba9bb407
Alina Karpulova <karpulova.alina@gmail.com>
2024-10-02 22:24:15 -0700

    Exercises ercycler view

--- app/src/main/java/com/example/zenith/activities/adapters/ExerciseRowAdapter.java
+++ app/src/main/java/com/example/zenith/activities/adapters/ExerciseRowAdapter.java
@@ -1,53 +1,74 @@
 package com.example.zenith.activities.adapters;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.zenith.R;
 import com.example.zenith.models.Exercise;
 
-public class ExerciseRowAdapter extends BaseAdapter {
-    Context context;
+public class ExerciseRowAdapter extends RecyclerView.Adapter<ExerciseRowAdapter.ViewHolder> {
     Exercise[] exercises;
-    private static LayoutInflater layoutInflater = null;
 
-    public ExerciseRowAdapter(Context context, Exercise[] exercises) {
-        this.context = context;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView name;
+        private final TextView label;
+        private final ImageView image;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            name = view.findViewById(R.id.exercise_name);
+            label = view.findViewById(R.id.exercise_category);
+            image = view.findViewById(R.id.exercise_img);
+        }
+
+        public TextView getLabel() {
+            return label;
+        }
+
+        public TextView getName() {
+            return name;
+        }
+
+        public ImageView getImage() {
+            return image;
+        }
+    }
+
+    public ExerciseRowAdapter(Exercise[] exercises) {
         this.exercises = exercises;
-        layoutInflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
+    // Create new views (invoked by the layout manager)
+    @NonNull
     @Override
-    public int getCount() {
-        return exercises.length;
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.exercise_row_item, viewGroup, false);
+        return new ViewHolder(view);
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public Object getItem(int i) {
-        return exercises[i];
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        viewHolder.getName().setText(exercises[position].getName());
+        viewHolder.getLabel().setText(exercises[position].getExerciseCategory().toString());
     }
 
     @Override
     public long getItemId(int i) {
-        return i;
+        return exercises[i].getId();
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        View vi = view;
-        if (vi == null) {
-            vi = layoutInflater.inflate(R.layout.exercise_row_item, null);
-        }
-        TextView name = (TextView) vi.findViewById(R.id.exercise_name);
-        TextView category = (TextView) vi.findViewById(R.id.exercise_category);
-
-        name.setText(exercises[position].getName());
-        category.setText(exercises[position].getExerciseCategory().toString());
-        return vi;
+    public int getItemCount() {
+        return exercises.length;
     }
 }
--- app/src/main/java/com/example/zenith/activities/screens/exercises/ExercisesFragment.java
+++ app/src/main/java/com/example/zenith/activities/screens/exercises/ExercisesFragment.java
@@ -9,8 +9,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.zenith.R;
+import com.example.zenith.activities.adapters.ExerciseRowAdapter;
+import com.example.zenith.models.Exercise;
+import com.example.zenith.models.ExerciseBodyPart;
+import com.example.zenith.models.ExerciseCategory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ExercisesFragment extends Fragment {
     public ExercisesFragment() {
@@ -30,8 +39,33 @@ public class ExercisesFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        List<Exercise> exercises = Arrays.asList(
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST),
+                new Exercise("Bench Press", ExerciseCategory.DUMBBELL, ExerciseBodyPart.CHEST)
+        );
 
-
+        ExerciseRowAdapter adapter = new ExerciseRowAdapter((Exercise[]) exercises.toArray());
+        RecyclerView recyclerView = view.findViewById(R.id.exercises_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setAdapter(adapter);
 
         return view;
 
